b2b81f63808e035b9cc9a6082266177a915c8a18
jiidaw <102522936+jiidaw@users.noreply.github.com>
2025-08-24 12:16:08 -0700

    Update TalUltimatePlugin.java

--- src/main/java/com/talultimate/TalUltimatePlugin.java
+++ src/main/java/com/talultimate/TalUltimatePlugin.java
@@ -3,13 +3,6 @@ package com.talultimate;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TalUltimatePlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        getLogger().info("TALUltimate loaded (base skeleton).");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("TALUltimate disabled.");
-    }
+    @Override public void onEnable() { getLogger().info("TALUltimate loaded!"); }
+    @Override public void onDisable() { getLogger().info("TALUltimate disabled!"); }
 }
