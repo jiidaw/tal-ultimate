4e8ff562bd8b982770c4b553cc93618864316947
jiidaw <102522936+jiidaw@users.noreply.github.com>
2025-08-24 12:09:11 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,8 @@
-plugins { java }
-
-group = "com.talultimate"
-version = "1.0.0"
-
 java {
-    toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
-}
-
-repositories {
-    mavenCentral()
-    maven(url = "https://repo.papermc.io/repository/maven-public/")
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-}
-
-tasks.withType<JavaCompile> { options.encoding = "UTF-8" }
