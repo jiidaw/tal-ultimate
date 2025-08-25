805f1b84c715a4463f106b7a4186952b13da6757
jiidaw <102522936+jiidaw@users.noreply.github.com>
2025-08-24 22:31:46 -0700

    Delete src/main directory

--- src/main/java/com/talultimate/TalUltimatePlugin.java
+++
@@ -1,9 +0,0 @@
-package com.talultimate;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class TalUltimatePlugin extends JavaPlugin {
-    @Override public void onEnable() { getLogger().info("TALUltimate loaded!"); }
-    @Override public void onDisable() { getLogger().info("TALUltimate disabled!"); }
-}
-
